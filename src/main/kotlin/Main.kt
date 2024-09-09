78e41c0ab9a7abc7475d94ed20c6be48dee74509
S0LARANA <s0169513@edu.kubsu.ru>
2024-09-09 19:29:18 +0300

    feat[lab 1]: add object creation from hash

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,20 @@
 fun main() {
     val student1 = Student(1, "Иванов", "Иван", "Иванович", phone = "+79123456789", email = "ivan@example.com")
     val student2 = Student(2, "Ивановв", "Иван", "Иванович", phone = "+79123456789")
+    val studentData = mapOf(
+        "id" to 3,
+        "lastName" to "Сидоров",
+        "firstName" to "Алексей",
+        "middleName" to "Владимирович",
+        "telegram" to "@alexsid",
+        "github" to "alexsid"
+    )
+    val student3 = Student.fromMap(studentData)
 
     println("Информация о студентах:")
-    student1.stringify()
-    student2.stringify()
+    println(student1.toString())
+    println(student2.toString())
+    println(student3.toString())
 
     student2.phone = "+79876543210"
     student2.email = "example@example.com"
--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,4 +1,4 @@
-class Student(
+data class Student(
     val id: Int,
     var lastName: String,
     var firstName: String,
@@ -9,12 +9,29 @@ class Student(
     var github: String? = null
     )
 {
-    fun stringify(){
-        println("Студент ID: $id")
-        println("ФИО: $lastName $firstName $middleName")
-        println("Телефон: ${phone ?: "-"}")
-        println("Telegram: ${telegram ?: "-"}")
-        println("Email: ${email ?: "-"}")
-        println("GitHub: ${github ?: "-"}")
+    override fun toString(): String{
+        return """
+Студент ID: $id
+ФИО: $lastName $firstName $middleName
+Телефон: ${phone ?: "Не указан"}
+Telegram: ${telegram ?: "Не указан"}
+Email: ${email ?: "Не указан"}
+GitHub: ${github ?: "Не указан"}
+        """
+    }
+
+    companion object {
+        fun fromMap(map: Map<String, Any?>): Student {
+            return Student(
+                id = map["id"] as? Int ?: throw IllegalArgumentException("ID обязателен"),
+                lastName = map["lastName"] as? String ?: throw IllegalArgumentException("Фамилия обязательна"),
+                firstName = map["firstName"] as? String ?: throw IllegalArgumentException("Имя обязательно"),
+                middleName = map["middleName"] as? String ?: throw IllegalArgumentException("Отчество обязательно"),
+                phone = map["phone"] as? String,
+                telegram = map["telegram"] as? String,
+                email = map["email"] as? String,
+                github = map["github"] as? String
+            )
+        }
     }
 }
