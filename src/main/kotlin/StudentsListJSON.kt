6841cc593306ce498728af99a624af6b1c5b5d42
S0LARANA <s0169513@edu.kubsu.ru>
2024-10-28 22:14:59 +0300

    fix[lab 3.3]: fix JSON parsing

--- src/main/kotlin/StudentsListJSON.kt
+++ src/main/kotlin/StudentsListJSON.kt
@@ -33,39 +33,6 @@ class StudentsListJSON(private val filePath: String) {
         }
     }
 
-    fun getStudentById(id: Int): Student? {
-        return students.find { it.id == id }
-    }
-
-    fun get_k_n_student_short_list(k: Int, n: Int): List<Student_short> {
-        val startIndex = (k - 1) * n
-        val endIndex = minOf(startIndex + n, students.size)
-        return students.subList(startIndex, endIndex).map { Student_short(it) }
-    }
-
-    fun sortByName() {
-        students.sortBy { "${it.lastName}${it.firstName}${it.middleName}" }
-    }
-
-    fun addStudent(student: Student) {
-        students.add(student)
-    }
-
-    fun replaceStudent(id: Int, newStudent: Student) {
-        val index = students.indexOfFirst { it.id == id }
-        if (index != -1) {
-            students[index] = newStudent
-        }
-    }
-
-    fun removeStudent(id: Int) {
-        students.removeIf { it.id == id }
-    }
-
-    fun get_student_short_count(): Int {
-        return students.size
-    }
-
     fun writeToJSON(filePath: String){
         try {
             val json = Json {
