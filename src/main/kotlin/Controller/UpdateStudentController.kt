6d6f2d1ac300942e12f30ca8a194f60491d76f50
S0LARANA <s0169513@edu.kubsu.ru>
2025-02-05 10:29:20 +0300

    feat[lab 7.3]: implement student updates

---
+++ src/main/kotlin/Controller/UpdateStudentController.kt
@@ -0,0 +1,29 @@
+package Controller
+
+import DBConnection.DatabaseManager
+import Model.Student
+import Model.StudentList
+import DBConnection.Students_list_DB
+import Model.main
+import javafx.stage.Stage
+import java.sql.Connection
+
+class UpdateStudentController(private val student: Student, private val students: StudentList, private val mainController: StudentListController, private val dbConnection:DatabaseManager) {
+    fun updateStudent(firstNameField: String, lastNameField: String, middleNameField: String) {
+        val updatedStudent = Student(
+            student.id,
+            lastNameField,
+            firstNameField,
+            middleNameField,
+            student.phone,
+            student.telegram,
+            student.email,
+            student.github
+        )
+        val studentDb = Students_list_DB(dbConnection)
+        studentDb.updateStudent(student.id, updatedStudent)
+        students.replaceStudent(student.id, updatedStudent)
+        System.out.println("Updated student: $updatedStudent")
+        mainController.updateTableData()
+    }
+}
